df1b82edc359c6fdbc6986b38b36a6ba88543f60
Aline Sayuri Kadekaro <aline.ask@gmail.com>
2024-05-26 22:20:57 -0300

    Initial commit

---
+++ app/src/main/java/br/com/movie_list/TelaEmAlta.kt
@@ -0,0 +1,22 @@
+package br.com.movie_list
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.movie_list.ui.theme.MovieListTheme
+
+
+class TelaEmAlta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_emalta)
+    }
+}
